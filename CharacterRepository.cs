9f61d4aee6f583d9760b04d1b98829b8a19d62e7
Mark Thomas <mark@theideaboutique.com>
2014-11-19 15:51:56 -0600

    characterrepository
    send to remote master CharacterRepository.cs

---
+++ CharacterRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernateTutorial.Domain;
+using NHibernate;
+
+namespace NHibernateTutorial
+{
+    public class CharacterRepository
+    {
+        public void Add(Character newCharacter)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Save(newCharacter);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        public Character GetCharacterByName(string name)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                var result = session.QueryOver<Character>().Where(x => x.Name == name).SingleOrDefault();
+                return result ?? new Character();
+            }
+        }
+
+        public void Update(Character newCharacter)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Update(newCharacter);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        public void Delete(Character newCharacter)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    session.Delete(newCharacter);
+                    transaction.Commit();
+                }
+            }
+        }
+
+    }
+}
