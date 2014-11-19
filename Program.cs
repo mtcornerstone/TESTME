28cac5712fe8c03e5a1d122ea004c8a7d2cbca22
Mark Thomas <mark@theideaboutique.com>
2014-11-19 16:03:13 -0600

    nhibernate and program
    Nhibernate and program

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernateTutorial.Domain;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace NHibernateTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadNHibernateCfg();
+
+            /* CRUD */
+            CharacterRepository repo = new CharacterRepository();
+
+            //CREATE!
+            var MikeAbyss = new Character { Name = "MikeAbyss", HealthPoints = 700, Mana = 10, Profession = "Knight" };
+            repo.Add(MikeAbyss);
+
+            //READ!
+            Character mike = repo.GetCharacterByName("MikeAbyss");
+
+            //UPDATE!
+            mike.Name = "Mike";
+            repo.Update(mike);
+
+            //DELETE!
+            repo.Delete(mike);
+
+            Console.ReadKey();
+        }
+
+        public static void LoadNHibernateCfg()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Character).Assembly);
+            new SchemaExport(cfg).Execute(true, true, false);
+        }
+
+    }
+}
